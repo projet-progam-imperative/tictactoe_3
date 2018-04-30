f78048fbc41620e4eb96e0e68cc2be087d9f4fbb
bobbywan <bobbywan@pc-76.home>
2018-04-30 16:50:20 +0200

    IA with simulation ver:1

--- grid.c
+++ grid.c
@@ -12,6 +12,21 @@ typedef struct{
     int last_grid;
 }board;
 
+int board_equal(board a, board b){
+    int i, j;
+    for(i = 0;i<9;i++){
+        for(j = 0;j<9;j++){
+            if(a.tab[i].tab[j] != b.tab[i].tab[j]){
+                return 0;
+            }
+        }
+    }
+    if(a.num_grid != b.num_grid) return 0;
+    if(a.player != b.player) return 0;
+    if(a.last_grid != b.last_grid) return 0;
+    return 1;
+}
+
 board init(board main_board){
     int i,j;
     for(i = 0;i<9;i++){
@@ -48,6 +63,13 @@ board turn(board main_board, int num_case){
     return main_board;
 }
 
+board turn_IA_alea(board main_board){
+    int IA_turn;
+    IA_turn = rand() % 9;
+    main_board = turn(main_board, IA_turn);
+    return main_board;
+}
+
 void draw(board main_board){
     int i,j;
     for(i = 0;i<9;i++){
@@ -62,7 +84,7 @@ int win(board main_board){
     int a, b, c, d, e, f, g, h;
     a = main_board.tab[main_board.last_grid].tab[0] + main_board.tab[main_board.last_grid].tab[1] + main_board.tab[main_board.last_grid].tab[2];
     b = main_board.tab[main_board.last_grid].tab[3] + main_board.tab[main_board.last_grid].tab[4] + main_board.tab[main_board.last_grid].tab[5];
-    c = main_board.tab[main_board.last_grid].tab[6] + main_board.tab[main_board.last_grid].tab[7] + main_board.tab[main_board.last_grid].tab[8];//___________________
+    c = main_board.tab[main_board.last_grid].tab[6] + main_board.tab[main_board.last_grid].tab[7] + main_board.tab[main_board.last_grid].tab[8];
     d = main_board.tab[main_board.last_grid].tab[0] + main_board.tab[main_board.last_grid].tab[3] + main_board.tab[main_board.last_grid].tab[6];
     e = main_board.tab[main_board.last_grid].tab[1] + main_board.tab[main_board.last_grid].tab[4] + main_board.tab[main_board.last_grid].tab[7];
     f = main_board.tab[main_board.last_grid].tab[2] + main_board.tab[main_board.last_grid].tab[5] + main_board.tab[main_board.last_grid].tab[8];
@@ -75,6 +97,53 @@ int win(board main_board){
         return -1;
     }
     return 0;
+}
+
+int decision(int taille, int* tab){
+    int max, i, index;
+    index = 0;
+    max = tab[0];
+    for(i=0;i<taille;i++){
+        if(max > tab[i]){
+            max = tab[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+board turn_IA_simulation(board simul){
+    int i,taille = 0;
+    board possible_tmp[9];
+    for(i=0;i<9;i++){
+        possible_tmp[i] = turn(simul,i);
+        if(board_equal(possible_tmp[i], simul) != 1){
+            taille += 1;
+        }
+    }
+    board possible[taille];
+    int index = 0;
+    for(i=0;i<9;i++){
+        if(board_equal(possible_tmp[i], simul) != 1){
+            possible[index] = possible_tmp[i];
+            index +=1;
+        }
+    }
+    int possible_val[taille] ;
+    for(i=0;i<taille;i++){
+        possible_val[i] = 0;
+        int j;
+        for(j=0;j<100;j++){
+            board test = possible[i];
+            int winner = 0;
+            while(winner == 0){
+                test = turn_IA_alea(test);
+                winner = win(test);
+            }
+            possible_val[i] += winner;
+        }
+    }
+    return simul = possible[decision(taille, possible_val)];
 }
 
 int game_JCJ(){
@@ -105,9 +174,27 @@ int game_JCP_alea(){
             scanf("%d",&c);
             plateau = turn(plateau,c);
         }else{
-            int IA_turn;
-            IA_turn = rand() % 9;
-            plateau = turn(plateau, IA_turn);
+            plateau = turn_IA_alea(plateau);
+        }
+        winner = win(plateau);
+    }
+    draw(plateau);
+    return winner;
+}
+
+int game_JCP_IA(){
+    board plateau;
+    plateau = init(plateau);
+    int c=0;
+    int winner = 0;
+    while(winner == 0){
+        if(plateau.player == 1){
+            draw(plateau);
+            printf("entrer le num de la case (0 -> 8)");
+            scanf("%d",&c);
+            plateau = turn(plateau,c);
+        }else{
+            plateau = turn_IA_simulation(plateau);
         }
         winner = win(plateau);
     }
@@ -117,7 +204,7 @@ int game_JCP_alea(){
 
 int main(){
     int winner;
-    winner = game_JCP_alea();
+    winner = game_JCP_IA();
     switch(winner){
         case 1:
             printf("Le joueur 1 a gagné\n");
